97a42ae90c305528408eff8084d509dbc7d15f03
Caleb <kalebsam@hotmail.com>
2024-06-13 11:37:44 -0500

    fixes merge

--- billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
+++ billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceRequestMapperImpl.java
@@ -23,6 +23,8 @@ public class InvoiceRequestMapperImpl implements InvoiceRequestMapper {
 
         Invoice invoice = new Invoice();
 
+
+
         invoice.setCustomerId( source.getCustomer() );
         invoice.setAmount( source.getAmount() );
         invoice.setDetail( source.getDetail() );
